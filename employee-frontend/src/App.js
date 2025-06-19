ce6571beee5e937a29eee34573a8b030113a6796
JOSE LUIS AUGUSTO ROMERO QUINTANA <U23238516@utp.edu.pe>
2025-06-19 09:54:23 -0500

    Employeeee prueba de API, reporte y front v1

---
+++ employee-frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Employees from './pages/Employees';
+
+function App() {
+  return (
+    <div className="App">
+      <Employees />
+    </div>
+  );
+}
+
+export default App;
